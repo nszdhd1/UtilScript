a1226b96b36ce83f0ad8565cce7b148f47ddd57c
yangtong <nszdhd_1>
2021-03-31 15:10:09 +0800

    add ida python

---
+++ exportCode.py
@@ -0,0 +1,24 @@
+import idaapi
+import idautils
+
+def decompile(func):
+    return str(idaapi.decompile(func))
+
+def main():
+    if not idaapi.init_hexrays_plugin():
+        return False
+    output = GetInputFile().split('.')[0]+'.cpp'
+    f = open(output,'a')
+    for segea in Segments():
+        for funcea in idautils.Functions(segea, SegEnd(segea)):
+            code = decompile(funcea)
+            f.write(code)
+    f.close()
+
+if main():
+    idaapi.term_hexrays_plugin()
+
+
+
+
+
